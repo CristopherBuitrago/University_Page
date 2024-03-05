4e24e27f7e921e9c6c0f46bbab422aa00773b448
Cristopher Daniel Ramirez Buitrago <145687056+CrisCamper@users.noreply.github.com>
2024-03-05 18:04:59 -0500

    Update subject.js

--- js/subject.js
+++ js/subject.js
@@ -73,11 +73,29 @@ const loadSubjectsForm = () => {
 
         <!-- cursos -->
         <div class="mb-3 row">
-            <label for="course" class="col-4 col-form-label">Curso</label>
-            <div class="col-7">
-                <input type="text" class="form-control" id="course" placeholder="Buscar..." required />
-            </div>
-            <button class=" col-9 btn btn-primary" id="search"><i class='bx bx-search bx-tada'></i></button>
+            <div class="form-group">
+            <label for="input-datalist">Timezone</label>
+            <input type="text" class="form-control" placeholder="Timezone" list="list-timezone" id="input-datalist">
+            <datalist id="list-timezone">
+                <option>Asia/Aden</option>
+                <option>Asia/Aqtau</option>
+                <option>Asia/Baghdad</option>
+                <option>Asia/Barnaul</option>
+                <option>Asia/Chita</option>
+                <option>Asia/Dhaka</option>
+                <option>Asia/Famagusta</option>
+                <option>Asia/Hong_Kong</option>
+                <option>Asia/Jayapura</option>
+                <option>Asia/Kuala_Lumpur</option>
+                <option>Asia/Jakarta</option>
+            </datalist>
+        </div>
+    
+    <script>
+        document.addEventListener('DOMContentLoaded', e => {
+            $('#input-datalist').autocomplete()
+        }, false);
+    </script>
         </div>
         </div>
 
