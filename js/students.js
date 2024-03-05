811a0054171c1f3d69df55c698bca7c9b3ff8787
CrisCamper <pandeabono2@gmail.com>
2024-03-05 11:06:16 -0500

    modified:   index.html 	modified:   js/students.js 	new file:   js/teachers.js

--- js/students.js
+++ js/students.js
@@ -1,44 +1,34 @@
+// Declaración de arrays para almacenar la lista de estudiantes y programas
 const studentsList = [];
 const programsList = [];
 
-const loadStudents = async () => {
-
+// Función para cargar datos desde una URL a una lista específica
+const fetchData = async (url, list) => {
     try {
-        studentsList.length = 0;
-        const response = await fetch('http://localhost:3000/alumnos');
-
+        list.length = 0;
+        const response = await fetch(url);
         if (!response.ok) {
-            throw new Error('Error al cargar estudiantes. Estado: ', response.status);
+            throw new Error(`Error al cargar datos desde ${url}. Estado: ${response.status}`);
         }
-        const students = await response.json();
-        studentsList.push(...students);
-
+        const data = await response.json();
+        list.push(...data);
     } catch (error) {
-        console.error("Error al cargar estudiantes", error.message);
+        console.error(`Error al cargar datos: ${error.message}`);
     }
-}
+};
+
+// Funciones para cargar estudiantes y programas
+const loadStudents = async () => {
+    await fetchData('http://localhost:3000/alumnos', studentsList);
+};
 
 const loadPrograms = async () => {
+    await fetchData('http://localhost:3000/programas', programsList);
+};
 
-    try {
-        programsList.length = 0;
-        const response = await fetch('http://localhost:3000/programas');
-
-        if (!response.ok) {
-            throw new Error('Error al cargar programas. Estado: ', response.status);
-        }
-        const programs = await response.json();
-        programsList.push(...programs);
-        console.log(programsList)
-
-    } catch (error) {
-        console.error("Error al cargar programas", error.message);
-    }
-}
-
+// Función para guardar un nuevo estudiante
 const saveStudent = async (newStudent) => {
     try {
-
         const response = await fetch('http://localhost:3000/alumnos', {
             method: 'POST',
             headers: {
@@ -48,20 +38,112 @@ const saveStudent = async (newStudent) => {
         });
 
         if (!response.ok) {
-            throw new Error('Error al crear el estudiante. Estado: ', response.status);
+            throw new Error(`Error al crear el estudiante. Estado: ${response.status}`);
         }
         const studentCreated = await response.json();
-
-
-        console.log('estudiante creado:', studentCreated);
-
+        console.log('Estudiante creado:', studentCreated);
     } catch (error) {
-        console.error("Error al cargar estudiantes", error.message);
+        console.error(`Error al crear el estudiante: ${error.message}`);
     }
-}
+};
 
+// Función para crear un nuevo estudiante
+const createStudent = async () => {
+    // Obtiene los valores de los campos del formulario y los asigna a un objeto nuevoEstudiante
+    const inputs = ['studentName', 'studentLastName', 'studentDocumentType', 'studentDocumentNumber', 'studentResidenCity', 'studentAdress', 'studentPhoneNumber', 'studentBirthDate', 'studentGenre', 'studentProgram'];
+    const newStudent = Object.fromEntries(inputs.map(id => [id, document.getElementById(id).value]));
+
+    // Asigna un ID al nuevo estudiante y lo guarda
+    newStudent.id = studentsList.length + 1;
+    await saveStudent(newStudent);
+    await loadStudents();
+
+    // Limpia los campos del formulario después de crear el estudiante
+    inputs.forEach(id => document.getElementById(id).value = '');
+
+    // Muestra una alerta de éxito
+    alert('¡Estudiante creado con éxito!');
+    return newStudent;
+};
+
+// Función para crear una celda de tabla con contenido dado
+const createCell = (content) => {
+    const cell = document.createElement("td");
+    cell.textContent = content;
+    return cell;
+};
+
+// Función para mostrar la lista de estudiantes
+const showList = async () => {
+    await loadStudents();
+    const studentForm = document.getElementById('students-form');
+    const studentsListed = document.getElementById('students-list');
+    
+    // Oculta el formulario y muestra la lista de estudiantes
+    studentForm.style.display = 'none';
+    studentsListed.style.display = 'block';
+
+    // Crea la estructura de la tabla para mostrar la lista de estudiantes
+    studentsListed.innerHTML = `
+        <div class="table-responsive">
+            <table class="table table-striped table-hover table-borderless table-primary align-middle">
+                <thead class="table-light">
+                    <caption>Estudiantes</caption>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>Programas</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider" id="info-students"></tbody>
+            </table>
+        </div>
+    `;
+
+    // Llena la tabla con los datos de los estudiantes
+    studentsList.forEach(({ id, nombre, apellido, programa_id }) => {
+        const row = document.createElement("tr");
+        row.classList.add("table-primary");
+        [id, nombre, apellido, programa_id].forEach(content => {
+            row.appendChild(createCell(content));
+        });
+        document.getElementById("info-students").appendChild(row);
+    });
+
+    // Agrega un botón para volver al formulario
+    const volverButton = document.createElement('button');
+    volverButton.setAttribute("class", "btn btn-danger");
+    volverButton.textContent = 'Volver al Formulario';
+    volverButton.addEventListener('click', volverFormulario);
+    studentsListed.appendChild(volverButton);
+
+};
+
+// Función para volver al formulario desde la lista de estudiantes
+const volverFormulario = () => {
+    const studentForm = document.getElementById('students-form');
+    const studentsListed = document.getElementById('students-list');
+
+    // Oculta la lista de estudiantes y muestra el formulario
+    studentsListed.style.display = 'none';
+    studentForm.style.display = 'block';
+};
+
+// Función para generar las opciones del menú desplegable de programas
+const generatePrograms = () => {
+    return programsList.map(program => `<option>${program.nombre}</option>`).join('');
+};
+
+// Función para cargar el formulario al cargar la página
 const loadStudentsForm = () => {
     const studentForm = document.getElementById('students-form');
+    const studentsListed = document.getElementById('students-list');
+    
+    // Oculta la lista de estudiantes al cargar el formulario
+    studentsListed.style.display = 'none';
+
+    // Crea el formulario
     studentForm.innerHTML = `
         <form>
         <!-- name -->
@@ -164,109 +246,9 @@ const loadStudentsForm = () => {
         </div>
         <hr>
 
-        <button type="button" onclick="createStudent()">Crear Cliente</button>
-        <button type="button" onclick="showList()">Ver Listado de Clientes</button>
+            <button class="btn btn-danger" class="col-2 col-form-label" type="button" onclick="showList()">Ver Listado de estudiantes</button>
+            <button class="btn btn-primary" class="col-6 col-form-label" type="button" onclick="createStudent()">Crear Estudiante</button>
+            
         </form>
     `;
-    const studentsListed = document.getElementById('students-list');
-    studentsListed.style.display = 'none';
-}
-
-const createStudent = async () => {
-    let nameInput = document.getElementById('studentName');
-    let lastNameInput = document.getElementById('studentLastName');
-    let docInput = document.getElementById('studentDocumentType');
-    let docNumbInput = document.getElementById('studentDocumentNumber');
-    let studCityInput = document.getElementById('studentResidenCity');
-    let studAdressInput = document.getElementById('studentAdress');
-    let studNumbInput = document.getElementById('studentPhoneNumber');
-    let studBirthInput = document.getElementById('studentBirthDate');
-    let studGenreInput = document.getElementById('studentGenre');
-    let studProgramInput = document.getElementById('studentProgram');
-
-    const name = nameInput.value;
-    const lastName = lastNameInput.value;
-    const docType = docInput.value;
-    const docNumb= docNumbInput.value;
-    const studCity = studCityInput.value;
-    const studAdress = studAdressInput.value;
-    const studNumb = studNumbInput.value;
-    const studBirth = studBirthInput.value;
-    const studGenre = studGenreInput.value;
-    const studProgram = studProgramInput.value;
-
-    const newStudent = {
-        id: studentsList.length + 1,
-        nombre: name,
-        apellido: lastName,
-        tipo_documento: docType,
-        numero_documento: docNumb,
-        ciudad_residencia: studCity,
-        direccion: studAdress,
-        telefono: studNumb,
-        fecha_nacimiento: studBirth,
-        sexo: studGenre,
-        programa_id: studProgram,
-    }
-
-
-    await saveStudent(newStudent);
-    await loadStudents();
-
-    nameInput= "";
-    lastNameInput= "";
-    docInput= "";
-    docNumbInput= "";
-    studCityInput= "";
-    studAdressInput= "";
-    studNumbInput= "";
-    studBirthInput= "";
-    studGenreInput= "";
-    studProgramInput= "";
-
-    alert('estudiante creado con éxito!');
-
-    //actulizarClientesEnFacturas();
-
-    return newStudent;
-
-}
-
-const showList = async () => {
-    await loadStudents();
-    const studentForm = document.getElementById('students-form');
-    const studentsListed = document.getElementById('students-list');
-
-    studentForm.style.display = 'none';
-    studentsListed.style.display = 'block';
-
-    const ul = document.createElement('ul');
-
-    for (const student of studentsList) {
-        const li = document.createElement('li');
-        li.textContent = `ID: ${student.id}, Nombre: ${student.nombre}, Apellido: ${student.apellido}, Programa: ${student.programa_id}`;
-        ul.appendChild(li);
-    }
-
-    studentsListed.innerHTML = '';
-    studentsListed.appendChild(ul);
-
-    const volverButton = document.createElement('button');
-    volverButton.textContent = 'Volver al Formulario';
-    volverButton.addEventListener('click', volverFormulario);
-    studentsListed.appendChild(volverButton);
-
-}
-
-const volverFormulario = () => {
-    const studentForm = document.getElementById('students-form');
-    const studentsListed = document.getElementById('students-list');
-
-    studentsListed.style.display = 'none';
-    studentForm.style.display = 'block';
-
-}
-
-const generatePrograms = () => {
-
-}   
+};
